413b4be4134392cc8140f522787a9031f153d48f
asitP9 <panda9asit@gmail.com>
2020-12-25 11:36:15 +0530

    initialised a dotnet file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlogManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
